a8ad3313dfae212bccccaf53d90f9641e64686d7
LHufton <lukehufton@gmail.com>
2024-01-08 01:52:06 -0600

    frontend running

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react'
+import React, { useState, useEffect } from 'react'
 import { Route, Routes } from 'react-router-dom'
 import { CheckSession } from './Services/Auth'
 import Nav from './Components/Nav'
