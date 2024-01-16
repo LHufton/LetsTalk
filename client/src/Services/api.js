a3073d8520bac0ce708bf4b960954db4231dc326
LHufton <lukehufton@gmail.com>
2024-01-16 02:44:38 -0600

    paths updated

--- client/src/Services/api.js
+++ client/src/Services/api.js
@@ -1,6 +1,6 @@
 import Axios from 'axios'
 
-export const BASE_URL = import.meta.env.HerokuApp || 'http://localhost:3001'
+export const BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:3001'
 
 const Client = Axios.create({ baseURL: BASE_URL })
 
